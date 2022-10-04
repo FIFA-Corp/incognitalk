23b3b2fb503321c73d16a16a4f0e832a0f4661e9
mohammad farizan <mohammadfarizan224@gmail.com>
2022-10-04 21:58:19 +0700

    feat: styling post-card and text-area

--- index.js
+++ index.js
@@ -1,30 +1,33 @@
 import { createPost } from "./module/posts/create-post.js";
 import { getPost } from "./module/posts/get-post.js";
 
-const formElement = document.querySelector('form');
-const postSectionElement = document.getElementsByClassName('post-section')[0];
+const formElement = document.querySelector("form");
+const postSectionElement = document.getElementsByClassName("post-section")[0];
 
 const renderPost = async () => {
   const posts = await getPost();
-  postSectionElement.innerHTML = posts.reverse().map(({ post }) => {
-    return `
-      <div>
+  postSectionElement.innerHTML = posts
+    .reverse()
+    .map(({ post }) => {
+      return `
+      <div class="post-card">
         <h1>${post}</h1>
       </div>
-    `
-  }).join("")
-}
+    `;
+    })
+    .join("");
+};
 
-formElement.addEventListener('submit', async (event) => {
+formElement.addEventListener("submit", async (event) => {
   try {
     event.preventDefault();
     const form = new FormData(formElement);
-    const postValue = form.get('post');
+    const postValue = form.get("post");
     const addPost = await createPost(postValue);
     renderPost();
   } catch (error) {
     console.error(error.message);
   }
-})
+});
 
 renderPost();
