14dc6a22d78b089e76a7988ebb0fbf5e11e91160
Fikri Alwan Ramadhan <fikriar93@gmail.com>
2022-10-07 14:33:36 +0700

    feat: restyling post card

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { createPost } from "./module/posts/create-post.js";
 import { getPost } from "./module/posts/get-post.js";
 
 const formElement = document.querySelector("form");
-const postSectionElement = document.getElementsByClassName("post-section")[0];
+const postSectionElement = document.getElementById("post-section");
 
 const renderPost = async () => {
   const posts = await getPost();
@@ -10,8 +10,12 @@ const renderPost = async () => {
     .reverse()
     .map(({ post }) => {
       return `
-      <div class="post-card">
-        <h1>${post}</h1>
+      <div class="py-5 px-7 bg-grey-second rounded-2xl mb-2 flex flex-col gap-4">
+        <div class="flex flex-col gap-1">
+          <time class="font-light text-xs text-white-second">5 Okt 2022</time>
+          <h1 class="font-medium text-lg text-white-primary">${post}</h1>
+        </div>
+        <p class="font-normal text-base text-primary">3 comments</p>
       </div>
     `;
     })
