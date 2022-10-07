7b44ab98710f1df8ba3ebdf6adf36c548f256260
Fikri Alwan Ramadhan <fikriar93@gmail.com>
2022-10-07 14:53:48 +0700

    feat: add login show total comments in post card Close #11

--- index.js
+++ index.js
@@ -8,14 +8,15 @@ const renderPost = async () => {
   const posts = await getPost();
   postSectionElement.innerHTML = posts
     .reverse()
-    .map(({ post }) => {
+    .map(({ post, comments }) => {
+      const lengthComment = comments.length;
       return `
       <div class="py-5 px-7 bg-grey-second rounded-2xl mb-2 flex flex-col gap-4">
         <div class="flex flex-col gap-1">
           <time class="font-light text-xs text-white-second">5 Okt 2022</time>
           <h1 class="font-medium text-lg text-white-primary">${post}</h1>
         </div>
-        <p class="font-normal text-base text-primary">3 comments</p>
+        <p class="font-normal text-base text-primary">${lengthComment > 0 ? lengthComment : 'Add'} ${lengthComment > 1 ? 'comments' : 'comment'}</p>
       </div>
     `;
     })
