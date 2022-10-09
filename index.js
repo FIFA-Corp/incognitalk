053531d0cf4769820228b3bc62ffdddeadd92709
Fikri Alwan Ramadhan <fikriar93@gmail.com>
2022-10-09 16:57:27 +0700

    feat: add function show all popular

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const postSectionElement = document.getElementById("post-section");
 const textAreaElement = document.getElementById("post");
 const submitButtonElement = document.getElementById("submit-button");
 const popularPostElement = document.getElementById("popular-post-items");
+const showAllPopularElement = document.getElementById("show-all-popular");
 
 dayjs.extend(window.dayjs_plugin_relativeTime);
 textAreaElement.addEventListener("input", () => {
@@ -20,26 +21,52 @@ textAreaElement.addEventListener("input", () => {
   }
 });
 
-const render = async () => {
+showAllPopularElement.addEventListener('click', () => {
+  render(true);
+});
+
+const render = async (isPopular = false) => {
   const posts = await getPost();
   const popularPostWithLimit = SortPopular(posts, 5);
-  postSectionElement.innerHTML = posts
-    .map(({ post, commentItems, createPostDate }) => {
-      const lengthComment = commentItems.length;
-      const postedAt = createPostDate;
-      const postedAtDisplay = dayjs(postedAt).fromNow();
-      return `
+
+  if (isPopular) {
+    const popularPost = SortPopular(posts);
+    postSectionElement.innerHTML = popularPost
+      .map(({ post, commentsCount, createPostDate }) => {
+        const postedAt = createPostDate;
+        const postedAtDisplay = dayjs(postedAt).fromNow();
+        return `
+      <div class="py-5 px-7 bg-grey-second rounded-2xl mb-2 flex flex-col gap-4">
+        <div class="flex flex-col gap-1">
+          <time class="font-light text-xs text-white-second">${postedAtDisplay}</time>
+          <h1 class="font-medium text-lg text-white-primary">${post}</h1>
+        </div>
+        <p class="font-normal text-base text-primary">${commentsCount > 0 ? commentsCount : "Add"
+          } ${commentsCount > 1 ? "comments" : "comment"}</p>
+      </div>
+    `;
+      })
+      .join("");
+  } else {
+    postSectionElement.innerHTML = posts
+      .map(({ post, commentItems, createPostDate }) => {
+        const lengthComment = commentItems.length;
+        const postedAt = createPostDate;
+        const postedAtDisplay = dayjs(postedAt).fromNow();
+        return `
       <div class="py-5 px-7 bg-grey-second rounded-2xl mb-2 flex flex-col gap-4">
         <div class="flex flex-col gap-1">
           <time class="font-light text-xs text-white-second">${postedAtDisplay}</time>
           <h1 class="font-medium text-lg text-white-primary">${post}</h1>
         </div>
         <p class="font-normal text-base text-primary">${lengthComment > 0 ? lengthComment : "Add"
-        } ${lengthComment > 1 ? "comments" : "comment"}</p>
+          } ${lengthComment > 1 ? "comments" : "comment"}</p>
       </div>
     `;
-    })
-    .join("");
+      })
+      .join("");
+
+  }
 
   popularPostElement.innerHTML = popularPostWithLimit.map(({ post, commentsCount }) => {
     return `
@@ -63,5 +90,7 @@ formElement.addEventListener("submit", async (event) => {
     console.error(error.message);
   }
 });
+
+
 
 render();
